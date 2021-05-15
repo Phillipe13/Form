887fe5c266da7c96b2f5268307405520f212233f
Phillipe Tibúrcio <pphillip13@hotmail.com>
2021-05-14 23:47:02 -0300

    adicionado forms restantes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import './App.css';
 import { Formik, Form, Field } from 'formik';
 import * as Yup from 'yup';
 
+
+
 const schema = Yup.object().shape({
   nome:Yup.string().required(),
   idade:Yup.number().min(16).required(),
   cpf:Yup.string().required(),
+  matricula:Yup.number.length(9).required(),
   curso:Yup.string().required(),
   endereco:Yup.string().required(),
-  numero:Yup.number().required()  
+  numero:Yup.number(),
+  complemento:Yup.string(),
+  bairro:Yup.string().required(),
+  cidade:Yup.string().required(),
+  estado:Yup.string().length(2).required(),
+  cep:Yup.string().required()
+
 })
 
 function App() {
@@ -66,10 +75,59 @@ function App() {
               )}
             </div>
             <div>
-              <label htmlFor="endereco">Idade</label>
+              <label htmlFor="endereco">Endereço</label>
               <Field id="endereco" name="endereco" type="text"/>
               {errors.endereco &&(
                 <div>{errors.endereco}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="matricula">Matricula</label>
+              <Field id="matricula" name="matricula" type="text"/>
+              {errors.matricula &&(
+                <div>{errors.matricula}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="numero">Número</label>
+              <Field id="numero" name="numero" type="number"/>
+              {errors.numero &&(
+                <div>{errors.numero}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="complemento">Complemento</label>
+              <Field id="complemento" name="complemento" type="text"/>
+              {errors.complemento &&(
+                <div>{errors.complemento}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="bairro">Bairro</label>
+              <Field id="bairro" name="bairro" type="text"/>
+              {errors.bairro &&(
+                <div>{errors.bairro}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="cidade">Cidade</label>
+              <Field id="cidade" name="cidade" type="text"/>
+              {errors.cidade &&(
+                <div>{errors.cidade}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="estado">Estado</label>
+              <Field id="estado" name="estado" type="text"/>
+              {errors.estado &&(
+                <div>{errors.estado}</div>
+              )}
+            </div>
+            <div>
+              <label htmlFor="cep">CEP</label>
+              <Field id="cep" name="cep" type="text"/>
+              {errors.cep &&(
+                <div>{errors.cep}</div>
               )}
             </div>
           </Form>
